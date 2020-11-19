8bad7766975d3d467b68f2d8dfa17756e5e6ab7c
AlejandriaA <51420143+AlejandriaA@users.noreply.github.com>
2020-11-19 17:55:31 -0500

    Tarea 4.2

--- Tarea4/ejercicios.js
+++ Tarea4/ejercicios.js
@@ -230,3 +230,253 @@ for (let i = 1; i <= numeroCiudades; i++) {
     console.log(`El total de ventas de la ciudad ${i} es : ${ventasCiudad}`);
 }
 console.log(`El total de ventas de la cadena es : ${ventasCadena}`); */
+
+/* //Ejercicio 4.1
+
+let salario = 1500;
+for (let i = 1; i <= 6; i++) {
+    console.log(`Salario año ${i}: ${salario}`);
+    salario += salario*0.1;
+}
+console.log(`Su salario al cabo de 6 años es : ${salario}`); */
+
+/* //Ejercicio 4.2
+
+let total = 0;
+let cantidadHamburguesas = +prompt(`¿Cuántas hamburguesas pedirá?`);
+for (let i = 1; i <= cantidadHamburguesas; i++) {
+    while (true) {
+        tipo = prompt(`¿La hamburguesa #${i} será (S)Sencilla, (D)Doble o (T)Triple?`).toLowerCase();
+        if (tipo == 's') {
+            total += 20;
+            break;
+        }
+        if (tipo == 'd') {
+            total += 25;
+            break;
+        }
+        if (tipo == 't') {
+            total += 28;
+            break;
+        }
+    }
+}
+while (true) {
+    formaPago = prompt(`¿Pagará con tarjeta de crédito?`).toLowerCase();
+    if (formaPago == 'si') {
+        total += total*0.05;
+        break;
+    }
+    if (formaPago == 'no') {
+        break;
+    }
+}
+console.log(`El total a pagar es : ${total}`); */
+
+/* //Ejercicio 4.3
+
+let cantidadPositivos = 0;
+let cantidadNegativos = 0;
+let cantidadCeros = 0;
+let cantidadNumeros = +prompt("Ingresa la cantidad de números");
+for (let i = 0; i < cantidadNumeros; i++) {
+    let numero = +prompt("Ingresa el número");
+    if (numero > 0) {
+        cantidadPositivos++;
+    } else if (numero < 0) {
+        cantidadNegativos++;
+    }
+    else {
+        cantidadCeros++;
+    }
+}
+console.log(`La cantidad de números positivos es : ${cantidadPositivos}`);
+console.log(`La cantidad de números negativos es : ${cantidadNegativos}`); 
+console.log(`La cantidad de ceros es : ${cantidadCeros}`);  */
+
+/* //Ejercicio 4.4
+
+let cantidadVerdes = 0;
+let cantidadRojos = 0;
+let cantidadBlancos = 0;
+let cantidadFocos = +prompt("Cantidad de focos : ");
+for (let i = 1; i <= cantidadFocos; i++) {
+    while (true) {
+        tipo = prompt(`Tipo de foco #${i} (V) Verde (R) Rojo (B) Blanco : `).toLowerCase();
+        if (tipo == 'v') {
+            cantidadVerdes++;
+            break;
+        }
+        if (tipo == 'r') {
+            cantidadRojos++;
+            break;
+        }
+        if (tipo == 'b') {
+            cantidadBlancos++;
+            break;
+        }
+    }
+}
+console.log(`La cantidad de focos verdes es : ${cantidadVerdes}`);
+console.log(`La cantidad de focos rojos es : ${cantidadRojos}`); 
+console.log(`La cantidad de focos blancos es : ${cantidadBlancos}`); */
+
+/* //Ejercicio 4.5
+
+let ahorroTotal = 0;
+let ahorroDia = 0.03;
+
+for (let i = 1; i <= 365; i++) {
+    console.log(`El ahorro del dia ${i} es : ${ahorroDia} pesos`);
+    ahorroDia = ahorroDia*3;
+    ahorroTotal += ahorroDia;
+}
+console.log(`El ahorro final es : ${ahorroTotal} pesos`); */
+
+//Ejercicio 4.6 == 4.1
+
+//Ejercicio 4.7 == 4.2
+
+/* //Ejercicio 4.8 
+
+let numeroArticulos = +prompt("Ingrese el número de artículos : ");
+let precioTotal = 0;
+for (let i = 1; i <= numeroArticulos; i++) {
+    let precioArticulo = +prompt(`Ingrese el precio del artículo ${i} : `);
+    if (precioArticulo >= 200) {
+        precioArticulo -= precioArticulo*0.15;
+    } else if (precioArticulo > 100) {
+        precioArticulo -= precioArticulo*0.12;
+    } else {
+        precioArticulo -= precioArticulo*0.10;
+    }
+    precioTotal += precioArticulo;
+}
+console.log(`El monto a pagar es : ${precioTotal}`); */
+
+/* //Ejercicio 4.9
+
+let años = +prompt("Ingrese el número de años : ");
+for (let i = 1; i <= años; i++) {
+    let ahorroAño = 0;
+    for (let j = 1; j <= 12; j++) {
+        ahorroAño += +prompt(`Ingrese el ahorro del mes #${j} del año #${i} : `);
+    }
+    console.log(`El ahorro total del año #${i} : ${ahorroAño+=ahorroAño*0.1}`);
+} */
+
+/* //Ejercicio 4.10
+
+let edadesEscuela = 0;
+let salones = +prompt("Ingrese el número de salones : ");
+for (let i = 1; i <= salones; i++) {
+    let edadesSalon = 0;
+    let estudiantes = +prompt(`Ingrese el número de estudiantes del salón #${i} : `);
+    for (let j = 1; j <= estudiantes; j++) {
+        edadesSalon += +prompt(`Ingrese la estatura del alumno #${j} del salón #${i} : `);
+    }
+    let promedioSalon = edadesSalon/estudiantes;
+    console.log(`Edad promedio del salón #${i} : ${promedioSalon}`);
+    edadesEscuela += promedioSalon;
+}
+console.log(`Edad promedio de la escuela : ${edadesEscuela/salones}`); */
+
+/* //Ejercicio 4.11
+
+let x = +prompt("Ingrese el valor de x : ");
+let n = +prompt("Ingrese el valor de n : ");
+ex = 0;
+for (let i = 0; i <= n; i++) {
+    fact = 1;
+    for (let j = i; j > 0; j--) {
+        fact *= j;
+    }
+    ex += Math.pow(x,i)/fact;
+}
+console.log(`El resultado es : ${ex}`); */
+
+/* //Ejercicio 4.12
+
+let total = 0;
+let billetes = +prompt("Ingrese la cantidad de billetes : ");
+for (let i = 1; i <= billetes; i++) {
+    billete = +prompt(`Ingrese el valor del billete #${i}: `);
+        switch (billete) {
+            case 10: case 20: case 50: case 100: case 200:
+                total += billete;
+                break;
+            default:
+                i--;
+                break;
+        }
+}
+let monedas = +prompt("Ingrese la cantidad de monedas : ");
+for (let i = 1; i <= monedas; i++) {
+    moneda = +prompt(`Ingrese el valor del moneda #${i}: `);
+        switch (moneda) {
+            case 5: case 2: case 1: case 0.5: case 0.2: case 0.1:
+                total += moneda;
+                break;
+            default:
+                i--;
+                break;
+        }
+}
+console.log(`El total de la caja registradora es : S/ ${total}`); */
+
+/* //Ejercicio 4.13
+
+let numeroVentas = +prompt("Ingrese el número de ventas : ");
+let total = 0;
+let ventasA = 0;
+let totalA = 0;
+let ventasB = 0;
+let totalB = 0;
+for (let i = 1; i <= numeroVentas; i++) {
+    let monto = +prompt(`Ingrese el monto de la venta #${i} : `);
+    if (monto <= 10000) {
+        total += monto;
+        ventasA++;
+        totalA += monto
+    } else if (monto < 20000) {
+        total += monto;
+        totalB += monto
+    } else {
+        total += monto;
+    }
+}
+console.log(`Las ventas por 10000 o menos fueron ${ventasA} y sumaron un monto de ${totalA}`); 
+console.log(`Las ventas por menos de 20000 fueron ${ventasB} y sumaron un monto de ${totalB}`);
+console.log(`El monto total de ventas es ${total}`); */
+
+/* //Ejercicio 4.15
+
+let trabajadores = +prompt("Ingrese el número de trabajadores : ");
+for (let i = 1; i <= trabajadores; i++) {
+    let trabajador = prompt(`Ingrese el nombre del trabajador #${i} : `);
+    let sueldo = 0;
+    do {
+        sueldo = 0;
+        let horas = +prompt(`Ingrese el número de horas laboradas : `);
+        let pago = +prompt(`Ingrese el pago por hora : `);
+        sueldo = horas*pago;
+        if (sueldo<=150) {
+            sueldo -= sueldo*0.05;
+        } else if (sueldo<300) {
+            sueldo -= sueldo*0.07;
+        }else {
+            sueldo -= sueldo*0.09;
+        } 
+    } while (sueldo<0 || sueldo>=450);
+    console.log(`El sueldo semanal de ${trabajador} es ${sueldo}`);
+} */
+
+/* //Ejercicio 4.16
+
+let cantidad = +prompt("Ingrese la cantidad de números : ");
+for (let i = 1; i <= cantidad; i++) {
+    let numero = +prompt("Ingrese el número : ");
+    console.log(`El cubo de ${numero} es ${numero*numero*numero}`);
+} */
+
+//Ejercicio 4.17 
